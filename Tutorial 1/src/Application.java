50ea46c76988e4ef2000ca250487c0811c6989fc
Quasar-light <t.d.sande@gmail.com>
2017-01-09 19:21:34 +0100

    Initial commit

---
+++ Tutorial 1/src/Application.java
@@ -0,0 +1,31 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber;
+		
+		myNumber = 24;
+		
+		long myLong = 5647;
+		double myDouble = 6.547;
+		byte myByte = 120;
+		char myChar = 'y';
+		
+		System.out.println(myNumber);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myByte);
+		System.out.println(myChar);
+		short myShort = 854;
+		float myFloat = 434.48f;
+		boolean myBoolean = false;
+		
+		
+		System.out.println(myShort);
+		System.out.println(myFloat);
+		System.out.println(myBoolean);
+				
+				
+	}
+
+}
