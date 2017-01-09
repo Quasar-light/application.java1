a16c4e0469d9b82702b02ff107570b1138e65d52
Quasar-light <t.d.sande@gmail.com>
2017-01-09 20:25:39 +0100

    2nd beginners java project

---
+++ Tutorial3/src/applicationT3.java
@@ -0,0 +1,17 @@
+
+public class applicationT3 {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "You are beautiful"; 
+		
+		String blank = " ";
+		
+		String name = "tee";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		System.out.println("You are beautiful" + " " + "tee");
+	}
+}
